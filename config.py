2e3557eb6d3c767c7a64f42a05937205a3b51fb6
stockbot_user_888 <qiaolihuang922@gmail.com>
2026-04-09 21:59:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ CHAT_ID = "7119676798"
 OPENAI_API_KEY = "sk-proj-" + "baPev12uI6bNVR4DNSYwxDHwR8QGUMaWqqb1ozFxHPoJIXDGefi2NAkT6cRu8y1iWSBAj8JgBnT3BlbkFJSfMow23qs6RQmHv3H1FINrbckSgaeAuKhGYcApOtRr-V97pPq6Oc7mnxHPi2NX3XDRLF_
 
 
-SUPABASE_URL = "https://your-project-id.supabase.co"
-SUPABASE_KEY = "your-anon-public-key"
+SUPABASE_URL = "https://mdfcihpqpubmaliulxsz.supabase.co"
+SUPABASE_KEY = "sb_publishable_JkofSfsB92sfOP9WqAZioA_Tm39zih-"
 
