443e73e3bcc65da4e8aa6657144908ea5cc0ebf9
stockbot_user_888 <qiaolihuang922@gmail.com>
2026-04-09 16:46:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-# ===== 🔒 永不修改區 =====
-TOKEN = "你的TOKEN"
-CHAT_ID = "你的CHAT_ID"
+
+# ===== 🔒 永不修改區（AI也不能動）=====
+TOKEN = "8714533132:AAGEAYs-Q-oZJDwwBwwuB0MPb27mqnDtzxs"
+CHAT_ID = "7119676798"
 OPENAI_API_KEY = "sk-proj-xxxx"
