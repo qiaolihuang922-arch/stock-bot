1fde5adb0c05affbd637191fa726da28c56a9a11
stockbot_user_888 <qiaolihuang922@gmail.com>
2026-04-09 16:47:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 # ===== 🔒 永不修改區（AI也不能動）=====
 TOKEN = "8714533132:AAGEAYs-Q-oZJDwwBwwuB0MPb27mqnDtzxs"
 CHAT_ID = "7119676798"
-OPENAI_API_KEY = "sk-proj-xxxx"
+
+OPENAI_API_KEY = "sk-proj-" + "baPev12uI6bNVR4DNSYwxDHwR8QGUMaWqqb1ozFxHPoJIXDGefi2NAkT6cRu8y1iWSBAj8JgBnT3BlbkFJSfMow23qs6RQmHv3H1FINrbckSgaeAuKhGYcApOtRr-V97pPq6Oc7mnxHPi2NX3XDRLF_VfzEA"
